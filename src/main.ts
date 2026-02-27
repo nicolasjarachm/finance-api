56c0fa3228647a60e099ad08eaf64747f382ab62
Nicolás Jara <nicolas.jarachm@gmail.comm>
2026-02-26 22:27:19 -0300

    funcionando : auth, JWT, Guard, password, ClassSerializerInterceptor activo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Interceptor es,
+  // Intercepta las respuestas de los controladores
+  // y las transforma en objetos serializables.
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
